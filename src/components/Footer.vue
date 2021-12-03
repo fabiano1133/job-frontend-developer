2f97e5e43aff5d721b40608858533a1f9b6c9377
fabiano1133 <fabiano.santos0@icloud.com>
2021-12-03 15:02:13 -0300

    build(componente): criando o componente Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>Yampi. Alguns Direitos Reservados</footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+footer {
+  background-color: #87f;
+  padding: 25px 0;
+  text-align: center;
+  font-size: 1.2rem;
+  color: #fff;
+  position: relative;
+  top: 75px;
+}
+</style>
